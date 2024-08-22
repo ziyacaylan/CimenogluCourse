0c64d11eaad2e8cec593aa1178dc65db5ee03672
Your Name <you@example.com>
2024-08-22 17:44:38 +0300

    PRJ-001: Proje oluşturuldu.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CimenogluCourse.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
