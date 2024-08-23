eab43f093238103afdf9e5a1bb2ddb716d3ef602
Your Name <you@example.com>
2024-08-23 23:09:54 +0300

    PRJ-002: Navbar , main ve footer ayarlandı

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [],
+    theme: {
+        extend: {
+            keyframes: {
+                slideDown: {
+                    "0%": { height: "0" },
+                    "100%": { height: "var(--height)" },
+                },
+                slideUp: {
+                    "0%": { height: "var(--height)" },
+                    "100%": { height: "0" },
+                },
+            },
+            animation: {
+                slideDown: "slideDown 0.5s ease-out forwards",
+                slideUp: "slideUp 0.5s ease-out forwards",
+            },
+        },
+    },
+    plugins: [],
+};
